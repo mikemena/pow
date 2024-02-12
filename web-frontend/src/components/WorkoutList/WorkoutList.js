6fe59d935ce7c04103ac6409c4b9f55f8a2bd705
tokyo <tokyolabs@protonmail.com>
2024-02-12 18:44:57 -0500

    wip

--- web-frontend/src/components/WorkoutList/WorkoutList.js
+++ web-frontend/src/components/WorkoutList/WorkoutList.js
@@ -19,3 +19,19 @@ const WorkoutList = ({ onChange }) => {
 };
 
 export default WorkoutList;
+
+
+export default function Exercise({ id, name, muscle, equipment, image }) {
+  return (
+    <div key={id} className='exercise'>
+      <img src={image} alt={name} className='exercise-image' />
+      <div className='exercise-details'>
+        <p className='exercise-title'>
+          {name} ({equipment})
+        </p>
+        <p className='exercise-muscle'>{muscle} </p>
+      </div>
+    </div>
+  );
+}
+
--- web-frontend/src/pages/Workouts/Workout.js
+++ web-frontend/src/pages/Workouts/Workout.js
@@ -1,11 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Workout.css';
 import WorkoutList from '../../components/WorkoutList/WorkoutList';
 
-const AddTemplatePage = () => {
+const WorkoutTemplatePage = () => {
   const [templateName, setTemplateName] = useState('');
+  const [workoutTemplates, setWorkoutTemplates] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   const navigate = useNavigate();
+
+  useEffect(() => {
+    fetch('http://localhost:9025/api/workout-templates/2')
+      .then(response => response.json())
+      .then(data => {
+        setWorkoutTemplates(data);
+        setIsLoading(false); // Finish loading after fetching exercises
+        console.log(data);
+      })
+      .catch(error => {
+        console.error('Failed to fetch workout templates:', error);
+        setIsLoading(false); // Finish loading even if there was an error
+      });
+  }, []);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
 
   const handleCreateNewWorkout = () => {
     // Redirect to the create workout page
@@ -21,9 +41,16 @@ const AddTemplatePage = () => {
         <h2 className='second-subtitle'>Templates</h2>
         <button onClick={handleCreateNewWorkout}>Create New Template</button>
       </div>
-      <WorkoutList />
+      {}
+      <WorkoutList
+        key={workoutTemplates.workout_id}
+        name={workoutTemplates.workout_name}
+        day_type={workoutTemplates.workout_day_type}
+        plan_type={workoutTemplates.plan_type}
+        difficulty_level={workoutTemplates.difficulty_level}
+      />
     </div>
   );
 };
 
-export default AddTemplatePage;
+export default WorkoutTemplatePage;
