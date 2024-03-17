e82d15c675990c9fc2831b38200166c4be02482e
tokyo <tokyolabs@protonmail.com>
2024-03-17 10:48:59 -0400

    wip

--- web-frontend/src/App.js
+++ web-frontend/src/App.js
@@ -3,12 +3,11 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/home/home';
 import ExercisesListPage from './pages/exercises/main/exercises';
-
 import ProgramPage from './pages/programs/main/programs';
 import CreateProgramPage from './pages/programs/create/program';
-import CreateWorkout from './pages/workoutTemplates/create/template';
-import CreateWorkoutDetails from './pages/workoutTemplates/create/templateDetails';
-import EditWorkout from './pages/workoutTemplates/edit/template';
+// import CreateWorkout from './pages/workoutTemplates/create/template';
+// import CreateWorkoutDetails from './pages/workoutTemplates/create/templateDetails';
+// import EditWorkout from './pages/workoutTemplates/edit/template';
 import ProgressPage from './pages/progress/main/progress';
 import ProfilePage from './pages/profile/main/profile';
 
@@ -21,14 +20,14 @@ function App() {
         <Route path='/' element={<HomePage />} />
         <Route path='/exercises' element={<ExercisesListPage />} />
 
-        <Route path='/create-workout' element={<CreateWorkout />} />
+        {/* <Route path='/create-workout' element={<CreateWorkout />} /> */}
         <Route path='/programs' element={<ProgramPage />} />
         <Route path='/create-program' element={<CreateProgramPage />} />
-        <Route
+        {/* <Route
           path='/create-workout-details'
           element={<CreateWorkoutDetails />}
-        />
-        <Route path='/edit-workout' element={<EditWorkout />} />
+        /> */}
+        {/* <Route path='/edit-workout' element={<EditWorkout />} /> */}
         <Route path='/progress' element={<ProgressPage />} />
         <Route path='/profile' element={<ProfilePage />} />
       </Routes>
