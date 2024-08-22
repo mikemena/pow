bfd234641fa8db3896eb958e6e814e5c83e4b071
tokyo <tokyolabs@protonmail.com>
2024-08-22 18:55:13 -0400

    Removed SET_WORKOUTS as its no longer used

--- web-frontend/src/reducers/activeWorkoutReducer.js
+++ web-frontend/src/reducers/activeWorkoutReducer.js
@@ -1,10 +1,9 @@
 import { actionTypes } from '../actions/actionTypes';
 
-function activeWorkoutReducer(state = null, action) {
+function activeWorkoutReducer(state, action) {
   switch (action.type) {
     case actionTypes.SET_ACTIVE_WORKOUT:
       return action.payload;
-
     default:
       return state;
   }
