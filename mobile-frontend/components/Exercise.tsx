91144e061bcb93646ccd6e81c60403c9f9e48e17
tokyo <tokyolabs@protonmail.com>
2024-10-03 21:49:38 -0400

    updated exercise set style

--- mobile-frontend/components/Exercise.tsx
+++ mobile-frontend/components/Exercise.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState, useMemo } from 'react';
+import React, { useContext, useState, useEffect, useMemo } from 'react';
 import {
   View,
   Text,
@@ -7,10 +7,11 @@ import {
   TouchableOpacity
 } from 'react-native';
 import Ionicons from '@expo/vector-icons/Ionicons';
+import * as Crypto from 'expo-crypto';
 import { ProgramContext } from '../src/context/programContext';
 import { useTheme } from '../src/hooks/useTheme';
 import { getThemedStyles } from '../src/utils/themeUtils';
-import { globalStyles } from '../src/styles/globalStyles';
+import { globalStyles, colors } from '../src/styles/globalStyles';
 import {
   Workout,
   Set,
@@ -36,10 +37,10 @@ const Exercise: React.FC<ExerciseProps> = ({
   const { state, addSet, updateSet, removeSet, updateWorkout } =
     useContext(ProgramContext);
   const workouts = state.workout.workouts;
-  console.log('workouts', workouts);
+  // console.log('workouts', workouts);
   const activeWorkout = state.workout.activeWorkout;
-  console.log('activeWorkout', activeWorkout);
-  console.log('initialWorkout', initialWorkout);
+  // console.log('activeWorkout', activeWorkout);
+  // console.log('initialWorkout', initialWorkout);
 
   const { mode } = state;
   const { state: themeState } = useTheme();
@@ -57,6 +58,12 @@ const Exercise: React.FC<ExerciseProps> = ({
     return workouts.find(w => w.id === initialWorkout.id) || initialWorkout;
   }, [workouts, initialWorkout]);
 
+  useEffect(() => {
+    if (workout) {
+      setLocalExercises(workout.exercises);
+    }
+  }, [workout]);
+
   const [localExercises, setLocalExercises] = useState(
     workout?.exercises || []
   );
@@ -69,7 +76,28 @@ const Exercise: React.FC<ExerciseProps> = ({
       return;
     }
 
+    // Call the context function to add the set
     addSet(workout.id, exerciseId);
+
+    // Update local state
+    setLocalExercises((prevExercises: ExerciseType[]) =>
+      prevExercises.map(ex =>
+        ex.catalog_exercise_id === exercise.catalog_exercise_id
+          ? {
+              ...ex,
+              sets: [
+                ...ex.sets,
+                {
+                  id: Crypto.randomUUID(),
+                  order: ex.sets.length + 1,
+                  weight: null,
+                  reps: null
+                }
+              ]
+            }
+          : ex
+      )
+    );
   };
 
   const handleUpdateSetLocally = (
@@ -135,7 +163,18 @@ const Exercise: React.FC<ExerciseProps> = ({
     }
   };
 
+  const findLocalSet = (
+    exerciseId: number | string,
+    setId: number | string
+  ) => {
+    const exercise = localExercises.find(
+      e => e.catalog_exercise_id === exerciseId
+    );
+    return exercise?.sets.find(s => s.id === setId);
+  };
+
   const renderSetInputs = (set: Set, setIndex: number) => {
+    const localSet = findLocalSet(exercise.catalog_exercise_id, set.id);
     if (mode === 'view') {
       return (
         <View key={setIndex} style={styles.setInfo}>
@@ -160,10 +199,12 @@ const Exercise: React.FC<ExerciseProps> = ({
             style={[
               globalStyles.input,
               styles.input,
-              { backgroundColor: themedStyles.primaryBackgroundColor }
+              {
+                backgroundColor: themedStyles.primaryBackgroundColor,
+                color: themedStyles.textColor
+              }
             ]}
-            value={mode === 'edit' ? set.weight?.toString() ?? '' : ''}
-            placeholder='Weight'
+            value={mode === 'edit' ? localSet?.weight?.toString() ?? '' : ''}
             placeholderTextColor={themedStyles.textColor}
             keyboardType='numeric'
             onChangeText={text =>
@@ -181,10 +222,12 @@ const Exercise: React.FC<ExerciseProps> = ({
             style={[
               globalStyles.input,
               styles.input,
-              { backgroundColor: themedStyles.primaryBackgroundColor }
+              {
+                backgroundColor: themedStyles.primaryBackgroundColor,
+                color: themedStyles.textColor
+              }
             ]}
-            value={mode === 'edit' ? set.reps?.toString() ?? '' : ''}
-            placeholder='Reps'
+            value={mode === 'edit' ? localSet?.reps?.toString() ?? '' : ''}
             placeholderTextColor={themedStyles.textColor}
             keyboardType='numeric'
             onChangeText={text =>
@@ -244,7 +287,7 @@ const Exercise: React.FC<ExerciseProps> = ({
           <Text
             style={[styles.exerciseMuscle, { color: themedStyles.textColor }]}
           >
-            {exercise.muscle}
+            {exercise.muscle} - {exercise.equipment}
           </Text>
         </View>
       </View>
@@ -270,7 +313,8 @@ const Exercise: React.FC<ExerciseProps> = ({
           onPress={() => handleAddSet(exercise)}
           style={[
             { backgroundColor: themedStyles.primaryBackgroundColor },
-            globalStyles.iconCircle
+            globalStyles.iconCircle,
+            styles.addSetButton
           ]}
         >
           <Ionicons
@@ -308,6 +352,7 @@ const styles = StyleSheet.create({
     padding: 10
   },
   exerciseIndex: {
+    fontFamily: 'Lexend',
     fontSize: 16,
     fontWeight: 'bold',
     marginRight: 12
@@ -317,13 +362,14 @@ const styles = StyleSheet.create({
     fontSize: 16
   },
   exerciseMuscle: {
+    fontFamily: 'Lexend',
     fontSize: 16
   },
   setInfo: {
     flexDirection: 'row',
     justifyContent: 'space-between',
-    marginBottom: 10,
-    paddingHorizontal: 12
+    // marginBottom: 5,
+    paddingHorizontal: 20
   },
   setText: {
     fontFamily: 'Lexend',
@@ -344,16 +390,8 @@ const styles = StyleSheet.create({
     marginLeft: 10
   },
   addSetButton: {
-    alignSelf: 'center',
-    marginTop: 10,
-    marginBottom: 10,
-    padding: 5,
-    borderRadius: 15,
-    width: 30,
-    height: 30,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#4CAF50'
+    marginHorizontal: 45,
+    marginBottom: 10
   },
   addSetButtonText: {
     color: 'white',
